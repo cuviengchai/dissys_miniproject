b133d185bee86b2bdec5c6387a858ef0b7effbc1
Pongpisit <pongpisit.tha@gmail.com>
2018-04-06 12:39:17 +0700

    Add User Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: String,
+    username: String,
+    password: String,
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
