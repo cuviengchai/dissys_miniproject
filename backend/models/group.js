5a95f066707e0193547f00ce72dc402f08bb558c
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-06 22:16:09 +0700

    added getAllGroup(), createGroup(), and fixed auth

--- backend/models/group.js
+++ backend/models/group.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const GroupSchema = new Schema({
-	id: Schema.ObjectId,
 	name: String,
 });
 module.exports = Group = mongoose.model('Group', GroupSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-	id: Schema.ObjectId,
 	name: String,
 	username: String,
 	password: String,
