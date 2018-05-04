2b4c1f8f87dd6bcc399cc90f1df5880bd19dcfb7
kritkrm <kritkrm@gmail.com>
2018-05-04 11:53:37 +0700

    LFSE

--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -23,7 +23,8 @@ class Message extends Component {
                 
                 <div style={{ float: this.props.isMe ? 'right' : 'left', height: '100%', fontSize: 18, cursor: 'pointer', display: 'inline-block' }} onMouseOver={this.onHover} onMouseOut={this.onOut}>
                     <div style={{ float: this.props.isMe ? 'right' : 'left', marginBottom: '24px' }}>
-                        {this.props.name}
+                        {`${this.props.name}`}
+                        <span style={{ color: 'gray', marginLeft: '8px' }}>{`${this.props.sentAt.split('T')[0]} ${this.props.sentAt.split('T')[1].substr(0,8)}`}</span>
                     </div>
                     <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', width: '100%', height: '64px', borderRadius: '8px', padding: '16px', marginTop: '32px'}}>{this.props.message}</div>
                 </div>
